d87d871dfb072e7e5efa5f5ef3c9e4cb49da2dff
adrianwithah <null>
2019-10-11 14:53:54 +0100

    Add logo

--- src/components/LocalDBForm.test.tsx
+++ src/components/LocalDBForm.test.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import LocalDBForm from './LocalDBForm';
-import { createMemoryHistory } from "history";
-import { Router } from "react-router";
-import {render, fireEvent} from '@testing-library/react'
 
-let container;
-
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-it('navigates to pathname equal to input to LocalDBForm', () => {
-  // Update variables to grant us access to the location
-  // and history objects in this test.
-  const history = createMemoryHistory();
-  const {container} = render(
-    <Router history={history}>
-      <LocalDBForm />
-    </Router>,
-  );
-
-  // Simulate input into the text field and press enter.
-  let testField = container.querySelector('input');
-  let form = container.querySelector('form');
-  fireEvent.change(testField, {target: { value: "asdf" }});
-  fireEvent.submit(form);
-
-  expect(history.location.pathname).toBe("/asdf")
-});
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LocalDBForm />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
