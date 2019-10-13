2553487712ae9448f1b31905610431e89319cf53
Matthew Yeo <my1817@ic.ac.uk>
2019-10-13 12:50:36 +0100

    Refactor provider functions out into DAGNode

--- src/utils/DAGNode.ts
+++ src/utils/DAGNode.ts
@@ -1,3 +1,5 @@
+import {Store} from 'orbit-db-store';
+
 export class DAGNode {
     nodeList: [DAGNode];
     hash: string;
@@ -5,5 +7,23 @@ export class DAGNode {
     constructor(hash, nodeList) {
         this.nodeList = nodeList;
         this.hash = hash;
+    }
+
+    static emptyDAG(): DAGNode {
+        return new DAGNode("EMPTY", []);
+    }
+
+    static createDAG(head: any, db: Store, depth: number = 10): DAGNode {
+        return depth > 0
+            ? new DAGNode(
+                head.hash,
+                (head.next
+                    ? head.next.map(node => this.createDAG(db.get(node), db, --depth))
+                    : [])
+            )
+            : new DAGNode(
+                head.hash,
+                []
+            );
     }
 }
