4a391369d5a3402c980265ba9371a0b123527267
LinuxOmen <qzc17@ic.ac.uk>
2019-10-13 17:04:17 +0100

    Revert landing page to show actual landing page
    qzc17

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
   Route
 } from 'react-router-dom';
 import Landing from 'src/pages/Landing';
-import GraphDisplay from './components/GraphDisplay';
 
 const App: React.FC = () => {
   return (
@@ -15,7 +14,7 @@ const App: React.FC = () => {
       <Router>
         <Switch>
           <Route exact path='/'>
-            <GraphDisplay />
+            <Landing />
           </Route>
         </Switch>
       </Router>
