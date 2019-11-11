b09cfa3bcc847c374ff884039600c0ab8d910857
adrianwithah <null>
2019-11-11 21:00:33 +0000

    Add counter store

--- src/components/databaseUi/CounterStoreUI.tsx
+++ src/components/databaseUi/CounterStoreUI.tsx
@@ -19,7 +19,7 @@ export default class CounterStoreUI implements DatabaseUIProvider {
     const incrementField = (opName, op) => {
       return () => (
         <div className={ToolbarStyle.inputFieldContainer}>
-          <form onSubmit={(e) => {e.preventDefault(); op()}}>
+          <form onSubmit={(e) => {e.preventDefault(); op(e)}}>
             <div className={ToolbarStyle.inputFieldRow}>
               <label>Amount: </label>
               <input
