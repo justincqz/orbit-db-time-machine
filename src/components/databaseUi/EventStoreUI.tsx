435866653402666bfd3503d4d6b846853bdc8fa7
adrianwithah <null>
2019-11-18 13:29:41 +0000

    Add JSON support for event store

--- src/components/databaseUi/EventStoreUI.tsx
+++ src/components/databaseUi/EventStoreUI.tsx
@@ -3,41 +3,104 @@ import React, { useState } from 'react';
 import ToolbarStyle from '../viewDatabase/Sidebar.module.css';
 import { Store } from "orbit-db-store";
 import DatabaseTableDisplay from "../DisplayComponents/DatabaseTableDisplay";
+import AceEditor from "react-ace";
+import Popup from "reactjs-popup";
+import "ace-builds/src-noconflict/mode-jsx";
+import PopupEditorStyles from './PopupEditor.module.css';
 
 export default class EventStoreUI implements DatabaseUIProvider {
   getSidebar: React.FC<Store> = ({ store }) => {
-    const [showAddFields, toggleAddFields] = useState(false);
-    const [input, setInput] = useState("");
+    const [editorStatus, setEditorStatus] = useState({data: "", open: false});
+    const [errorStatus, setErrorStatus] = useState({error: false, msg:""});
+    const [activeField, setActiveField] = useState("");
 
-    const inputFields = () => {
-      return (<div className={ToolbarStyle.inputFieldContainer}>
-      <form onSubmit={(e) => {e.preventDefault(); store.add(input)}}>
-        <div className={ToolbarStyle.inputFieldRow}>
-          <label>Value: </label>
-          <input
-            className={ToolbarStyle.inputField}
-            type="text"
-            onChange={(e) => setInput(e.target.value)}
-          />
-        </div>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: '0.5vh'}}>
-          <input type="submit" value="Add" className={ToolbarStyle.inputFieldSubmit}/>
-        </div>
-      </form>
-    </div>);
+    function handleEditorCancel() {
+      setEditorStatus({...editorStatus, open: false});
+    }
+
+    async function handleEditorSubmit() {
+      let data;
+      try {
+        data = JSON.parse(editorStatus.data);
+      } catch (e) {
+        // Cannot be parsed as JSON. Pass as string instead.
+        data = editorStatus.data
+      }
+      
+      // Can put any JSON-stringifyable data. 
+      try {
+        await store.add(data);
+        setEditorStatus({...editorStatus, open: false});
+      } catch (error) {
+        setEditorStatus({...editorStatus, open: true});
+        setErrorStatus({error: true, msg: error});
+      }
+    }
+
+    const addEventEditor = () => {
+      return () => (
+        <Popup open={editorStatus.open}
+                 closeOnDocumentClick={false}
+                 contentStyle={{backgroundColor:"#0A3464FF"}}>
+          <div>
+            <AceEditor
+              mode="json"
+              theme="solarized_dark"
+              fontSize="2vh"
+              onChange={(data) => {
+                setEditorStatus({data: data, open: true});
+                setErrorStatus({...errorStatus, error:false});
+              }}
+              name="aceEditor"
+              value= {editorStatus.data}
+              width="50vw"
+              height="40vh"
+              editorProps={{ $blockScrolling: true }}
+            />
+            { errorStatus.error
+              ?
+              <div className={PopupEditorStyles.errorDiv}>
+                <label>{errorStatus.msg}</label>
+              </div>
+              :
+              <div className={PopupEditorStyles.buttonBar}>
+                <button className={ToolbarStyle.editButton} onClick={handleEditorCancel}>Cancel</button>
+                <button className={ToolbarStyle.editButton} onClick={handleEditorSubmit}>Submit</button>
+              </div>
+            }
+          </div>
+        </Popup>
+      );
     };
 
-    return (<div>
-      <div className={ToolbarStyle.buttonBar}>
-        <button className={ToolbarStyle.addButton} onClick={() => toggleAddFields(!showAddFields)}>Add</button>
+    const opTypes = {
+      Add: addEventEditor()
+    };
+
+    const op = "Add";
+
+    return (
+      <div>
+        <div className={ToolbarStyle.buttonBar}>
+          <button
+            className={ToolbarStyle.addButton}
+            onClick={() => {
+              setEditorStatus({...editorStatus, open: true});
+              setActiveField(op);
+            }}
+            key={op}
+          >
+            {op}
+          </button>
+        </div>
+        {opTypes[activeField] ? opTypes[activeField]() : null}
       </div>
-      {showAddFields ? inputFields() : null}
-    </div>);
+    );
   }
 
   getDataDisplay: React.FC<any> = ({ index }) => {
     let filteredData = index.get().map((data) => {
-      return {"value" : data.payload.value};
+      return {"value" : JSON.stringify(data.payload.value)};
     });
 
     return <DatabaseTableDisplay data={filteredData.reverse()} />
@@ -46,7 +109,7 @@ export default class EventStoreUI implements DatabaseUIProvider {
   getTooltipMsg(entry): string {
     return (
       `Operation: ${entry.payload.op}
-      Value: ${entry.payload.value}`);
+      Value: ${JSON.stringify(entry.payload.value)}`);
   }
   
   getTooltipTitle(entry): string {
