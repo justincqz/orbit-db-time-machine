f48fa332dc8190fab8dc70cd5849d3f51e974d35
Hannes Hertach <hanneshertach@gmail.com>
2019-11-22 20:28:24 +0000

    Add documentation to logger class

--- logger/src/Logger.ts
+++ logger/src/Logger.ts
@@ -2,17 +2,37 @@ import Store from 'orbit-db-store';
 import { D3Data, getTreeAtSplit } from './model/D3DataType';
 import DAGNode from './model/DAGNode';
 import JoinEvent from './model/JoinEvent';
+import JoinStorageProvider from './providers/JoinStorageProvider';
+import LocalStoreageJoinProvider from './adapters/LocalJoinStorageAdapter';
 
+/**
+ * Class to log join events. The events can later be read by the time machine.
+ * By default, this uses a local storage provider.
+ */
 export default class Logger {
   private readonly store;
   private readonly storageProvider;
 
-  constructor(store: Store) {
+  /**
+   * Create a new logger
+   * @param store The orbitdb database to listen for events on
+   * @param storageProvider Optionally provide an custom storage provider.
+   */
+  constructor(store: Store, storageProvider?: JoinStorageProvider) {
     this.store = store;
-
+    if (storageProvider != null) {
+      this.storageProvider = storageProvider;
+    } else {
+      this.storageProvider = new LocalStoreageJoinProvider();
+    }
   }
 
-  start(cb?) {
+  /**
+   * Start the logger. This starts listening for join events and writing them
+   * to the storage provider.
+   * @param cb Optional callback function. Invoked when a join event is detected.
+   */
+  start(cb?: () => void) {
     this.store.events.on('replicated', () => {
       // Record the join event
       this.recordJoinEvent(this.saveHeadsAsD3Data(this.store._oplog));
@@ -23,6 +43,10 @@ export default class Logger {
     })
   }
 
+  /**
+   * Record a new join event to the storage provider using the passed oplog.
+   * @param newLog The current oplog
+   */
   private recordJoinEvent(newLog: any) {
     // Finds the earliest split node and returns a tree with it as root.
     let D3DAG: D3Data = this.saveHeadsAsD3Data(newLog.getHeads());
@@ -34,6 +58,11 @@ export default class Logger {
     }
   }
 
+  /**
+   * Write the current oplog heads to d3data
+   * @param heads the array of heads from the oplog
+   * @returns the d3data
+   */
   private saveHeadsAsD3Data(heads: any[]): D3Data {
     let DAGheads = DAGNode.createDAG(heads);
     let D3DAG = DAGheads[0].toD3Data(Infinity);
